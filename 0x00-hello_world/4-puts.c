696eccc9ce3953b500e1fd2a216a51af5973db70
natalia2807 <andreamorales200228@gmail.com>
2021-06-17 19:26:47 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ *main - funcion que imprime un mensajde
+ *
+ *Return: Always 0 (sucess)
+*/
 int main(void)
 {
-puts("programing is like building a multilingualpuzzle");
+puts("Programing is like building a multilingual puzzle");
 return (0);
 }
