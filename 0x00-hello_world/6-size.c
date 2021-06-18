17090d1709e20b5b6ab4897637a10b17b930ac7d
natalia2807 <andreamorales200228@gmail.com>
2021-06-17 20:44:18 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
 int main(int argc, char *argv[])
 {
-printf("Size of a char: %zd bytes\n", sizeof(char));
-printf("Size of an int: %zd bytes\n", sizeof(int));
-printf("Size of a long int: %zd bytes\n", sizeof(long));
-printf("Size of a long long int: %zd bytes\n", sizeof(long long));
-printf("Size of a float: %zd bytes\n", sizeof(float));
+printf("Size of a char: %zd byte(s)\n", sizeof(char));
+printf("Size of an int: %zd byte(s)\n", sizeof(int));
+printf("Size of a long int: %zd byte(s)\n", sizeof(long));
+printf("Size of a long long int: %zd byte(s)\n", sizeof(long long));
+printf("Size of a float: %zd byte(s)\n", sizeof(float));
 return (0);
 }
