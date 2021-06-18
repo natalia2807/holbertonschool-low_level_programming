d9404716a74ec2ad07d32bbeb928434d90f10ad3
natalia2807 <andreamorales200228@gmail.com>
2021-06-18 12:36:09 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 * main - prints "and the piece of art is useful" - Dora Korpar, 2015-10-19,
 *               followed by a new line, to the standard error.
 *
-* Return Always 1 if sucess.
+* Return: Always 1 if sucess.
 */
 int main(void)
 {
