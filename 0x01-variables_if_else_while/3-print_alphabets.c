fe9868ce0c153423afc122210b28cc419595d5d2
natalia2807 <andreamorales200228@gmail.com>
2021-06-20 20:54:57 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+  char A[25] = "abcdefghijklmnñopqrstuvwxyz";
+  char ch = 'G';
+  for (int i = 0; i< 25; i++)
+    {
+      putchar(A[I]);
+    }
+  for (int i = 0; i < 25; i++);
+  {
+    putchar (toupper(A[i]));
+  }
+  return (0);
+}
