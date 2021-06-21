8e9a068e36d08bc4cc2b5e77a9be3eff85b2a9cd
natalia2807 <andreamorales200228@gmail.com>
2021-06-20 19:17:35 -0700

    f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*betty style doc for function maind goes there*/
+/*betty style doc for function main goes there*/
 /**
  * maint- Entry point
  *
