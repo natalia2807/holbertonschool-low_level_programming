13d0b327aa9e4e43b16be914a1db9f5473fad689
natalia2807 <andreamorales200228@gmail.com>
2021-06-21 16:45:20 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,13 @@
 */
 int main(void)
 {
-int A[11] = {0,1,2,3,4,5,6,7,8,9};
+int A[10] = {0,1,2,3,4,5,6,7,8,9};
 int i = 0;
-while (i < 11)
+while (i < 10)
 {
-char number = A[i]+'0';
-putchar(number);
+putchar(A[i]+ '0');
 i++;
 }
+putchar('\n');
 return (0);
 }
