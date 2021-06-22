d89efe0a19a6f3e53066a03212ce4812a248f28b
natalia2807 <andreamorales200228@gmail.com>
2021-06-21 19:30:24 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (sucess)
+*/
+int main(void)
+{
+int i = 1;
+while (i < 100)
+{
+putchar((i/10)+'0');
+putchar((i%10)+'0');
+i++
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
