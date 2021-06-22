13c6c7e59bc5187b82c08c5214521051983d5991
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 13:14:53 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,12 +12,12 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+    r = islower('H');
+    putchar(r + '0');
+    r = islower('o');
+    putchar(r + '0');
+    r = islower(108);
+    putchar(r + '0');
+    putchar('\n');
     return (0);
 }
