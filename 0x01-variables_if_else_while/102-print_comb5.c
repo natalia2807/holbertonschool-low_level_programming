b84ad579db7422766bb48f72a41fc40d174ec15d
natalia2807 <andreamorales200228@gmail.com>
2021-06-21 21:53:31 -0700

    com5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,32 @@
-#include <stdio.h>
+#include <stdio.h<
 /**
- * main - Entry point      
- *
- * Return: Always 0 (sucess)
- */
+*main - Entry point
+*
+* Return: Always 0 (sucess)
+*/
 int main(void)
 {
 int num1 = 0;
-	while ( num1 <= 98)
-	{
-    int num2 = num1 + 1;
-		while ( num2 <= 99)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99){
-				break;
-                }
-
-			putchar(',');
-			putchar(' ');
-            num2++;
-		}
-        num1++;
-	}
-
+while (num1 <= 98)
+{
+int num2 = num1 + 1;
+while (num2 <= 99)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 == 98 && num2 == 99)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+num2++;
+}
+num1++;
+}
 putchar('\n');
-
-	return (0);
+return (0);
 }
