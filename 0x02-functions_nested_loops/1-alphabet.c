eb46b1cf5b40cd856dd05d4a3fa9900ed4fe5833
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 09:53:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet(void)
+ {
+ char a;
+ a=a;
+ while(c <= 'z')
+ {
+ _putchar(a);
+ a++;
+ }
+ _putchar('n');
+ return (0);
+ }
