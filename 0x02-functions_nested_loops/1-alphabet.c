5217c1f59fcd42ceaa682ab66776290827a065fb
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 12:46:17 -0700

    x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,17 @@
-
 #include <stdio.h>
-
-
-
 /**
 * main - print_alphabet  - this funtion print the alphabet
 *
 * Return: Always 0.
 */
-
 void  print_alphabet(void)
-
 {
 char n;
 n = 'a';
-
-while(n <= 'z')
+while (n <= 'z')
 {
-
 putchar(n);
 n++;
 }
 putchar('\n');
-
- }
+}
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,20 @@
-
-
 #include <stdio.h>
 /**
 * main - print_alphabet  - this funtion print the alphabet
 *
 * Return: Always 0.
 */
-
 void  print_alphabet_x10(void)
 {
-
 int n, a;
-a=0;
-while(a<10)
+a = 0;
+while (a < 10)
 {
-for(n='a'; n<='z'; n++){
+for (n = 'a'; n <= 'z'; n++)
+{
 putchar(n);
-
 }
 a++;
 putchar('\n');
-
 }
-
- }
- 
+}
