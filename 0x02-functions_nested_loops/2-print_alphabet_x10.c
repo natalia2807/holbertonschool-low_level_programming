7a3f3f1394a7d48a118251c6609db97be984880e
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 12:19:28 -0700

    h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+/**
+* main - print_alphabet  - this funtion print the alphabet
+*
+* Return: Always 0.
+*/
+
+void  print_alphabet(void)
+{
+
+int n, a;
+a=0;
+while(a<11)
+{
+for(n='a'; n<='z'; n++){
+putchar(n);
+
+}
+a++;
+putchar('\n');
+
+}
+
+ }
+ 
