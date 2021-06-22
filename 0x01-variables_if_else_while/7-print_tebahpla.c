7f42a87f775be2186548441ba91437d2a499e7e8
natalia2807 <andreamorales200228@gmail.com>
2021-06-21 17:05:09 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,13 @@
 */
 int main(void)
 {
-char A[25] = "abcdefghijklmnopqrstuvwxyz\n";
-int i = 25;
+char A[25] = "abcdefghijklmnopqrstuvwxyz";
+int i = 24;
 while (i > 0)
 {
 putchar(A[i]);
 i--;
 }
+putchar('\n'); 
 return (0);
 }
