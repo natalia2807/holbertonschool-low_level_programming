f5140dc68d079969ce270b92cb14c8928d66686d
natalia2807 <andreamorales200228@gmail.com>
2021-06-21 18:51:00 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
 */
 int main(void)
 {
-int A[10] = {0,1,2,3,4,5,6,7,8,9};
+int A[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 int i = 0;
 while (i < 10)
 {
-putchar(A[i]+ '0');
-if (A[i]!=9)
+putchar(A[i] + '0');
+if (A[i] != 9)
 {
 putchar(',');
 putchar(' ');
