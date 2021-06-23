ee83478ce11b6ddb98d32257e9465fd0a3de6569
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 20:11:17 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*_isalpha - a function that checks for lowercase character
+*@c: single letter input
+*
+* Return: Always 0 (sucess)
+*/
+int _abs(int);
+{
+ if (numero < 0)
+    {
+        return numero * -1;
+    }
+    return numero;
+}
