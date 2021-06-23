aea18cbf1518ac6fcc9f9945cd6a978c06ac948a
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 19:48:04 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 
 if (n > 0)
 {
-printf("+");
+printf("+",n);
 return (1);
 }
 else if(n == 0)
 {
-printf("0");
+printf("0",n);
 return (0);
 }
 else
 {
-printf("-");
+printf("-",n);
 return (-1);
 }
 }
