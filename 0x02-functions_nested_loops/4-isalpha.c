d1a2ba3666ec234524beb5d642307de6d97a0443
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 18:39:08 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+    char m=n;
+	if (m >= 'a' && m <= 'z'){
+		return (1);
+    }else{
+		return (0);
+    } 
+}
