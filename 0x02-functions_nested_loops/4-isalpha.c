6656aa0707ccf0dfdc684505ce3aa493f4727a4a
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 19:18:21 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+*_isalpha - a function that checks for lowercase character
+*@c: single letter input
+*
+* Return: Always 0 (sucess)
+*/
 int _isalpha(int c)
 {
-    char m=c;
-	if (m >= 'a' && m <= 'z'){
-		return (1);
-    }else{
-		return (0);
-    } 
+char m = c;
+if ((m >= 'a' && m <= 'z') || (m >= 'A' && m <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
