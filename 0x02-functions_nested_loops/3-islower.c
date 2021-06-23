417600e97465a934a61e446bcecd741d22bcbe99
natalia2807 <andreamorales200228@gmail.com>
2021-06-22 18:21:36 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-* main - check the code for Holberton School students.
+* _islower - a function that checksfor lowercase character
 *
-* Return: Always 0.
+* Return: Always 0 (sucess)
 */
 int _islower(int n)
 {
