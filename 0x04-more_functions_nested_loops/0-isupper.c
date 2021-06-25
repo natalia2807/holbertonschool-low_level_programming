5f5d773f193caabf911aca7fea94cd1bf1e2ab23
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 16:24:43 -0700

    issuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+char b;
+if (b = 'A'; b<='z')
+{
+putchar(n);
+return (1);
+}
+else 
+{
+return (0); 
