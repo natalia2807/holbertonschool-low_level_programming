f54ef5c5e86a8a1d01844f2d9be02d2b945f2cee
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 19:53:32 -0700

    most

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,18 +3,30 @@
  * more_numbers -  function that prints 10 times the numbers, from 0 to 14.
  *
  * Return: nothing.
- *//
+ */
 void more_numbers(void)
 {
 int n1, n2;
-
-for (n1 = 0; n1 < 10; n1++)
-for (n2 = 0; n2 <= 14; n2++)
+n1 = 0;
+while (n1 < 10)
 {
-if (n2 > 9)
-_putchar((n2 / 10) + '0');
-_putchar((n2 % 10) + '0');
+n2 = 0;
+while (n2 <= 14)
+{
+int n3 = 0;
+if (n2 <= 9)
+{
+n3 = n2;
 }
-_putchar('\n');
+else
+{
+n3 = n2 % 10;
+putchar((n2 / 10) + '0');
+}
+putchar(n3 + '0');
+n2++;
+}
+putchar('\n');
+n1++;
 }
 }
