a3511ad8f37deabe784e30779b5affa20682e59d
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 18:57:26 -0700

    most

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ for (n1 = 0; n1 < 10; n1++)
 for (n2 = 0; n2 <= 14; n2++)
 {
 if (n2 > 9)
-putchar((n2 / 10) + '0');
-putchar((n2 % 10) + '0');
+_putchar((n2 / 10) + '0');
+_putchar((n2 % 10) + '0');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
