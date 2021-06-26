6c043adea5e5b47e97de3d2fb87610e4c41ea9b6
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 18:27:37 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,16 @@
 #include "holberton.h"
-
 /**
- * mul - function that multiplies two integers.
- * @a: first input value to check
- * @b: Sec input value to check
- * Return: int.
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ *
+ * Return: nothing.
  */
 void print_numbers(void)
 {
-	int n = 0;
-
-	if (n < 10)
-	{
-		_putchar(n + '0');
-		
-	}
-	_putchar('\n');
+int n = 0;
+if (n < 10)
+{
+putchar(n + '0');
+n += 1;
+}
+_putchar('\n');
 }
