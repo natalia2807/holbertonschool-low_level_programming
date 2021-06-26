e8efce86b63272719a14ac1bd03b48b34a267665
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 19:59:04 -0700

    print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ * @n: input value to check
+ * Return: nothing.
+ */
+void print_line(int n)
+{
+int c = 0;
+
+while (c < n)
+{
+if (n > 0)
+{
+_putchar(95);
+n += 1;
+}
+else
+_putchar('\n');
+}
+_putchar('\n');
+}
