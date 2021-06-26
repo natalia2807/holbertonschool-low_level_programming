e82be58d9ef7902ee743b54e028d9e6d1caf1e7c
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 21:19:23 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - function that prints a square, followed by a new line
+ * @size: size of triangle
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+int b, cua, extr;
+
+if (size > 0)
+{
+for (b = 0; b < size; b++)
+{
+for (cua = 0; cua < size; cua++)
+{
+extr = (size - b) - 1;
+if (cua < extr)
+_putchar(' ');
+else
+_putchar(40);
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
+
