e36b8e4d34a1edc15f1147fefd5fb2b90f52c0f2
natalia2807 <andreamorales200228@gmail.com>
2021-06-25 18:11:33 -0700

    digit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- * _isdigit - function that checks for a digit (0 through 9).
- * @c: input value to check.
+ * mul - function that multiplies two integers.
+ * @a: first input value to check
+ * @b: Sec input value to check
  * Return: int.
  */
 int mul(int a, int b)
 {
-
-return (a*b);
+return (a * b);
 }
