8a577c01f79c1066e56c80284e4a1ed7c4fcfca3
natalia2807 <andreamorales200228@gmail.com>
2021-06-28 12:24:40 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*_rev - print a string
+*@s: string to find length of
+*
+*Return: void
+*/
+void print_rev(char *s)
+{
+int a = 0;
+while (*(s + a))
+{
+_putchar(*(s + a));
+a--;
+}
+putchar ('\n');
+}
