00fd4a081a76a2ee4e63f13a65a72d2738673c24
natalia2807 <andreamorales200228@gmail.com>
2021-06-28 15:10:47 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int str = 1;
+    if(*(str >=0 && str<=10))
+    {
+    putchar (str);
+    }
+    putchar ('\n');
+
+}
