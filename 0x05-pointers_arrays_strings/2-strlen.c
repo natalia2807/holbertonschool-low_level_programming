4cb7e87e81f1d74f1959483815a2bf39b8bbbdbe
natalia2807 <andreamorales200228@gmail.com>
2021-06-28 12:00:20 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: string to find length of
+*
+*Return: length of s
+*/
+int _strlen(char *s)
+{
+int a = 0;
+while (*(s + a))
+a++;
+return (a);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -30,3 +30,4 @@ int main(void);
 void print_number(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
