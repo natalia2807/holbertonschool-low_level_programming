802f5f9a47df765161c5b6ed4ea2c668c57723a9
natalia2807 <andreamorales200228@gmail.com>
2021-06-28 14:54:25 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int a = 0, b = 0;
+	char str[500];
+
+	while (*(s + a))
+	{
+		*(str + a) = *(s + a);
+		i++;
+	}
+	a = a - 1;
+	while (a >= 0)
+	{
+		*(s + a) = *(str + b);
+		b++;
+		a--;
+	}
+}
