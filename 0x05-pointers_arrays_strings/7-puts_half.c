5933f28ba4860a27932958128102f327aecff513
natalia2807 <andreamorales200228@gmail.com>
2021-06-28 15:31:12 -0700

    puts-half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+ char *str;
+
+    str = "56789";
+    printf(str);
+    return (0);
+}
